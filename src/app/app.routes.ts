389913bf92c89801ea02daa9f9fd5558859ba0d0
Daniela Giovanella <dannnigiovanella@gmail.com>
2026-05-19 16:47:23 +0200

    Skapat routes till vardera "undersida"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+//Importerar komponenter
+import { Home } from './pages/home/home';
+import { Courses } from './pages/courses/courses';
+import { Schedule } from './pages/schedule/schedule';
+import { CourseDetails } from './pages/course-details/course-details';
+import { NotFound } from './pages/not-found/not-found';
+
+export const routes: Routes = [
+     {
+    path: "", component: Home
+  },
+  {
+    path: "courses", component: Courses
+  },
+  {
+    path: "schedule", component: Schedule
+  },
+  {
+    path: "details", component: CourseDetails
+  },
+  {
+    path: "**", component: NotFound
+  }
+]; 
+
