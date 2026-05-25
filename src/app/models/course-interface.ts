4aba97b935cd2318b4b517f08dfac579c9d7ad7c
Daniela Giovanella <dannnigiovanella@gmail.com>
2026-05-25 15:56:34 +0200

    Uppdaterat interface

--- src/app/models/course-interface.ts
+++ src/app/models/course-interface.ts
@@ -4,12 +4,8 @@
 export interface CourseInterface {
 
     courseCode: string;
-    subjectCode: string;
-    level: string;
-    progression: string;
     courseName: string;
     points: number;
-    institutionCode: string;
     subject: string;
     syllabus: string;
 
