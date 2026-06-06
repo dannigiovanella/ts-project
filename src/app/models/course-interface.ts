0bfad7e865f73a1ce6e595c678c3e6948f7c933f
Daniela Giovanella <dannnigiovanella@gmail.com>
2026-06-07 01:56:10 +0200

    Fixat småfel i CSS som gav valideringsfel

--- src/app/models/course-interface.ts
+++ src/app/models/course-interface.ts
@@ -2,11 +2,9 @@
 //Visar hur varje kursobjekt ska se ut (baserat på api)
 
 export interface CourseInterface {
-
     courseCode: string;
     courseName: string;
     points: number;
     subject: string;
     syllabus: string;
-
 }
